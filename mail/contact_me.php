4a59ea1a5edecd0889620fa090919ea51c18b6e5
Ro P <rohpanu@hotmail.com>
2016-07-12 17:27:27 +0000

    July 12th 2016  Added carousels. TODO- Pictures, Test Contact Sheet, Acquire Domain Name, Color Scheme

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -20,7 +20,7 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 $to = 'rohpanu@hotmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Site Contact:  $name - $subject";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: ropanu@rpanuga.github.io\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "From: noreply@rpanuga.github.io\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";   
 mail($to,$email_subject,$email_body,$headers);
 return true;         
